4a25ec0def666a4090ff65370dc3276010f2be73
s3ponia <slava.shlyaga@gmail.com>
2019-12-07 14:59:50 +0300

    modify DominoBlock and Board

--- src/cpp/DominoBlock.cpp
+++ src/cpp/DominoBlock.cpp
@@ -4,12 +4,31 @@
 
 #include "../headers/DominoBlock.h"
 
-DominoBlock::DominoBlock(char f, char s) : first_(f), second_(s) {}
+DominoBlock::DominoBlock(ValueType f, ValueType s) : first_(f), last_(s) {}
 
-char DominoBlock::first() const {
+DominoBlock::ValueType DominoBlock::first() const {
     return first_;
 }
 
-char DominoBlock::second() const {
-    return second_;
+DominoBlock::ValueType DominoBlock::last() const {
+    return last_;
+}
+
+bool operator==(const DominoBlock &block1, const DominoBlock &block2) {
+    return block1.first_ == block2.first_ && block2.last_ == block1.last_;
+}
+
+size_t HashDominoBlock::operator()(const DominoBlock &block) const {
+    return (static_cast<size_t>(block.first()) << sizeof(decltype(block.first()))) |
+           static_cast<size_t>(block.last());
+}
+
+DominoBlock SwapFields(DominoBlock const &block) {
+    return {block.last(), block.first()};
+}
+
+std::string ToString(DominoBlock const &block) {
+    return "{" +
+           std::to_string(block.first()) + ", " + std::to_string(block.last()) +
+           "}";
 }

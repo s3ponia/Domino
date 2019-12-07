e3fdc863f900cb2f19051bab4d4c70004cf5322e
s3ponia <slava.shlyaga@gmail.com>
2019-12-07 15:02:18 +0300

    modify RealPlayer

--- src/headers/RealPlayer.h
+++ src/headers/RealPlayer.h
@@ -7,20 +7,30 @@
 
 #include "IPlayer.h"
 #include "Boneyard.h"
-#include <vector>
+#include <set>
+#include <unordered_set>
 #include <utility>
 
 class RealPlayer : public IPlayer {
 public:
-    RealPlayer(std::vector<DominoBlock> start_hand, Boneyard &boneyard);
+    enum class StepMode : uint8_t;
+    using HandType = std::unordered_set<DominoBlock, HashDominoBlock>;
+
+    RealPlayer(HandType start_hand, Boneyard &boneyard_);
 
     bool step(Board &) override;
+
+    const HandType &hand() const;
+
+    void SetStep(StepMode const &, DominoBlock const &);
 
 private:
     std::pair<bool, DominoBlock> GetDominoBlock();
 
-    std::vector<DominoBlock> hand_;
-    Boneyard &boneyard;
+    HandType hand_;
+    Boneyard &boneyard_;
+    StepMode step_mode_;
+    DominoBlock step_block_;
 };
 
 
